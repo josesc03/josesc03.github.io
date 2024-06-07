99241715e8137dccd94ca065b77e3734d14269fc
josesc03 <160251045+josesc03@users.noreply.github.com>
2024-06-07 23:41:42 +0200

    Update scripts de registro dni password

--- registroDNIPASSWORD/scripts.js
+++ registroDNIPASSWORD/scripts.js
@@ -1,22 +1,70 @@
+const lletres = [ "T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"];
+
+var dni = document.getElementById('dni')
+var password = document.getElementById('password');
+var confirmPassword = document.getElementById('confirmPassword');   
+var errorMessage = document.getElementById('errorMessage');
+
 document.getElementById('registrationForm').addEventListener('submit', function(event) {
-    
-    var dni = document.getElementById('dni');
-    console.log(dni.value);
-    var password = document.getElementById('password').value;
-    var confirmPassword = document.getElementById('confirmPassword').value;
-    var errorMessage = document.getElementById('errorMessage');
-    
-    if (password !== confirmPassword) {
+
+    if (!comprobarPassword()) {
         event.preventDefault();
         errorMessage.textContent = 'Las contraseñas no coinciden.';
-    } else if (!dni.value.match(/^\d{8}[a-zA-Z]{1}$/)) {
+    } else if (!validarDNI()) {
         event.preventDefault();
         errorMessage.textContent = 'DNI incorrecto.';
     } else {
         alert('Formulario enviado correctamente!');
         errorMessage.textContent = '';
-        setTimeout(() => {
-        }, 100);
-        
     }
 });
+
+
+dni.addEventListener('focusout', () => {
+    
+    if (!validarDNI()) {
+        dni.style.color = '#b20000'
+        errorMessage.textContent = 'DNI incorrecto.';
+    } else if (password.value !== confirmPassword.value) {
+        errorMessage.textContent = 'Las contraseñas no coinciden.';
+    } else {
+        errorMessage.textContent = '';
+    }
+});
+
+dni.addEventListener('focusin', () => {
+        dni.style.color = ''
+        errorMessage.textContent = '';
+});
+
+confirmPassword.addEventListener('focusout', () => {
+    if (!comprobarPassword()) {
+        errorMessage.textContent = 'Las contraseñas no coinciden.';
+    } else if (!validarDNI()) {
+        errorMessage.textContent = 'DNI incorrecto.';
+    } else {
+        errorMessage.textContent = '';
+}
+});
+
+function validarDNI() {
+    if (!dni.value.match(/^\d{8}[a-zA-Z]$/)) {
+        return false;
+    }
+
+    let numeroCorrecto = false;
+    var letraDni = dni.value.charAt(8);
+    lletres.forEach(char => {
+        if (letraDni.toUpperCase() == char) {
+            numeroCorrecto = true;
+        }
+    });
+    return numeroCorrecto;
+}
+
+function comprobarPassword() {
+    if (password.value == confirmPassword.value) {
+        return true;
+    }
+    return false;
+};
